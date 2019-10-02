eda19aee547fe61db6476a00965cdea5b6ff61bc
HeronCendrethe <heron.cendrethe@gmail.com>
2019-10-02 16:40:19 -0300

    Modos Quase Prontos

---
+++ WindowsFormsApplication2/TelaAusente2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class TelaAusente2 : Form
+    {
+        public TelaAusente2()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TelaAusente2_Load(object sender, EventArgs e)
+        {
+            radioButton4.Checked = true;
+        }
+    }
+}
---
+++ WindowsFormsApplication2/TelaDisponivel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class TelaDisponivel : Form
+    {
+        
+        TelaAusente telaAusente = new TelaAusente();
+        public TelaDisponivel()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_MouseMove(object sender, MouseEventArgs e)
+        {
+            ToolTip toolTip1 = new ToolTip();
+           
+            label1.Text = "Neste modo você ira escolher\n todas as ações a serem tomadas.";
+            label1.Visible = true;
+        }
+
+        private void label1_MouseLeave(object sender, EventArgs e)
+        {
+            ToolTip toolTip1 = new ToolTip();
+
+            label1.Text = "Disponível";
+            label1.Visible = true;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TelaDisponivel telaDisponivel = new TelaDisponivel();
+            this.telaAusente.Show();
+            this.Hide();
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+
